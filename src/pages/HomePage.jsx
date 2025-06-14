9fabf03d866f9ec42d0f7c2309de2dc47b657f12
Roman Serdiuk <roman.srdk21@gmail.com>
2025-06-14 15:04:08 -0400

    HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from "react";
+import { getTrendingMovies } from "../services/MoviesApi";
+
 function HomePage() {
-  return <div></div>;
+  const [trandingMovies, setTrendingMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      const movies = await getTrendingMovies();
+      // console.log(movies);
+      setTrendingMovies(movies);
+    };
+
+    fetchTrendingMovies();
+  }, []);
+
+  return (
+    <>
+      <h1>Tranding today</h1>
+      <ul>
+        {trandingMovies.map(({ id, title }) => (
+          <li key={id}>{title}</li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default HomePage;
